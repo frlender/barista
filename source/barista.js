226ab7183cedd8b97e3f75736aa7d921bf72f4dd
Corey Flynn <flynn.corey@gmail.com>
2013-07-10 14:53:24 -0400

    small doc update in PertSearchBar

--- source/barista.js
+++ source/barista.js
@@ -1907,7 +1907,7 @@ PertSearchBar = Backbone.View.extend({
 										});
 			},
 
-			// custom highlighter argument to display matched types
+			// custom highlighter argument to display matched types.  Display type aliases for known pert_types
 			highlighter: function(item){
 				if (self.cell_lines.indexOf(item) != -1){
 					return '<div><span class="label" style="background-color: #CC79A7">Cellular Context</span>  ' + item  +  '</div>';
