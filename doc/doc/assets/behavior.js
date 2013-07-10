c2aeaf2664bcaeaa190e0ec8c5a67dbaabb2b6c8
Corey Flynn <flynn.corey@gmail.com>
2013-07-09 21:23:44 -0400

    sorting bug in tickview

--- doc/doc/assets/behavior.js
+++ doc/doc/assets/behavior.js
@@ -463,6 +463,22 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           },
           "depth": 2,
           "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "singleLineComment": ["//"],
+              "name": "JavaScript"
+            },
+            "sourcePath": "/Users/cflynn/Code/barista/source/utils/arrayAverage.js",
+            "projectPath": "source/utils/arrayAverage.js",
+            "targetPath": "utils/arrayAverage",
+            "title": "arrayAverage"
+          },
+          "depth": 2,
+          "outline": []
         }
       ]
     }, {
