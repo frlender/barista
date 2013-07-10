7688d7299336c0ad15361605d82a447a41d74c60
Corey Flynn <flynn.corey@gmail.com>
2013-07-09 21:07:21 -0400

    fixed sorting bug in tickview

--- source/views/views.main.js
+++ source/views/views.main.js
@@ -1677,8 +1677,8 @@ TickView = Backbone.View.extend({
 		// grab data from the model and sort it according to the values in the object
 		var data_array = _.pairs(this.model.get('data_object'));
 		data_array.sort(function(a,b){
-			if (a < b) return 1;
-			if (a > b) return -1;
+			if (a[1] < b[1]) return 1;
+			if (a[1] > b[1]) return -1;
 			return 0;
 		});
 		var keys = [];
