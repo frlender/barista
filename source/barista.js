6d25be18b8de2cbf023ddcf26d30d1dee1ec9ab4
Corey Flynn <flynn.corey@gmail.com>
2013-07-09 21:23:44 -0400

    sorting bug in tickview

--- source/barista.js
+++ source/barista.js
@@ -21,6 +21,17 @@ CMapPertTypeAlias = function(input_type){
 		default:
 			return {name: input_type, acronym: input_type};
 	}
+};
+// # **arrayAverage**
+
+// a utility function to to take the average of an array of numeric values
+
+//		//evaluates to 2
+//		var a = arrayAverage([1,2,3]);
+function arrayAverage (arr){
+	return _.reduce(arr, function(memo, num){
+		return memo + num;
+	}, 0) / arr.length;
 }
 // # **CellCountModel**
 
@@ -2078,8 +2089,8 @@ TickView = Backbone.View.extend({
 		// grab data from the model and sort it according to the values in the object
 		var data_array = _.pairs(this.model.get('data_object'));
 		data_array = data_array.sort(function(a,b){
-			if (a[1] < b[1]) return 1;
-			if (a[1] > b[1]) return -1;
+			if (arrayAverage(a[1]) < arrayAverage(b[1])) return 1;
+			if (arrayAverage(a[1]) > arrayAverage(b[1])) return -1;
 			return 0;
 		});
 		console.log(data_array);
--- source/utils/utils.main.js
+++ source/utils/utils.main.js
@@ -21,4 +21,15 @@ CMapPertTypeAlias = function(input_type){
 		default:
 			return {name: input_type, acronym: input_type};
 	}
+};
+// # **arrayAverage**
+
+// a utility function to to take the average of an array of numeric values
+
+//		//evaluates to 2
+//		var a = arrayAverage([1,2,3]);
+function arrayAverage (arr){
+	return _.reduce(arr, function(memo, num){
+		return memo + num;
+	}, 0) / arr.length;
 }
