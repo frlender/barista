ae499e4f7206ee45426a65177b9955676a67a756
Corey Flynn <flynn.corey@gmail.com>
2014-01-07 16:02:00 -0500

    fixed CellLineage and CellHistology Dataset api calls

--- barista.main.js
+++ barista.main.js
@@ -5288,7 +5288,7 @@ Barista.Datasets = _.extend(Barista.Datasets,
 			remote: {
 				// set the remote data source to use cellinfo with custom query params
 				url: ['http://api.lincscloud.org/a2/cellinfo?',
-					  'q={"lincs_status":{"$in":["core_cline","core_pline"]},"cell_id":{"$regex":"%QUERY", "$options":"i"}}',
+					  'q={"lincs_status":{"$in":["core_cline","core_pline"]},"cell_histology":{"$regex":"%QUERY", "$options":"i"}}',
 					  '&l=10',
 					  '&s={"cell_id":1}'].join(''),
 				
@@ -5426,7 +5426,7 @@ Barista.Datasets = _.extend(Barista.Datasets,
 			remote: {
 				// set the remote data source to use cellinfo with custom query params
 				url: ['http://api.lincscloud.org/a2/cellinfo?',
-					  'q={"lincs_status":{"$in":["core_cline","core_pline"]},"cell_id":{"$regex":"%QUERY", "$options":"i"}}',
+					  'q={"lincs_status":{"$in":["core_cline","core_pline"]},"cell_lineage":{"$regex":"%QUERY", "$options":"i"}}',
 					  '&l=10',
 					  '&s={"cell_id":1}'].join(''),
 				
