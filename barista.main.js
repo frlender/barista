442fe7eb84e4c764115fe0bfd090fa0b0131214f
Corey Flynn <flynn.corey@gmail.com>
2014-01-14 10:12:24 -0500

    updated getEmSizeInPixels to select from body only

--- barista.main.js
+++ barista.main.js
@@ -5515,7 +5515,7 @@ Barista.arrayAverage = function arrayAverage (arr){
 
 // a utility function to find the size of 1em for the given element id
 Barista.getEmSizeInPixels = function(id) {
-    var el = document.getElementById(id);
+    var el = document.body;
     return Number(getComputedStyle(el, "").fontSize.match(/(\d+)px/)[1]);
 }
 // # **BarPlotModel**
