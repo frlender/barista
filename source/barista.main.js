6c83cfddbaa46f5274a891fb6de2cf2f14d8ced4
Corey Flynn <flynn.corey@gmail.com>
2014-01-15 14:47:15 -0500

    post ajax response filtering in CellMutationDataset

--- source/barista.main.js
+++ source/barista.main.js
@@ -499,6 +499,11 @@ Barista.Datasets = _.extend(Barista.Datasets,
 					var datum_list = [];
 					var auto_data = response;
 
+					// filter the list based on the query
+					console.log(this.query);
+					var re = new RegExp(this.query,"i");
+					auto_data = _.filter(auto_data,function(mutation){return re.match(mutation);});
+
 					// build a list of datum objects
 					auto_data.forEach(function(item){
 						var datum = {
