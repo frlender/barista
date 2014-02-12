202c1cbc1195a2175d87c05240f05372986757dd
Corey Flynn <flynn.corey@gmail.com>
2014-02-12 16:42:58 -0500

    make setUserKey synchronous

--- source/barista.main.js
+++ source/barista.main.js
@@ -596,8 +596,6 @@ Barista.getEmSizeInPixels = function(id) {
 // 
 // 1.  {string}  **key**  The user_key to use or a path to a JSON file containing a user_key attribute, defaults to *""*
 Barista.setUserKey = function(key) {
-	// grab the user_key from the url given by string passed in 'key' or set the string itself
-	// as user_key if an ajax call to the string fails
 	// configure ajax calls to add the user key parameter on calls to api.lincscloud.org
 	$.ajaxPrefilter(function( options, originalOptions, jqXHR ){
 		var re = new RegExp('api.lincscloud.org');
@@ -605,7 +603,9 @@ Barista.setUserKey = function(key) {
 			options.data = $.param($.extend(originalOptions.data,{user_key:Barista.user_key}));
 		}
 	});
-	
+
+	// grab the user_key from the url given by string passed in 'key' or set the string itself
+	// as user_key if an ajax call to the string fails
 	var key_request = $.ajax(key,{dataType: 'json',async: false});
 	key_request.done(function(res){
 		Barista.user_key = res.user_key;
