ca692491ef7eedac8ddf1871c165cbde18a9796f
Corey Flynn <flynn.corey@gmail.com>
2014-02-12 12:27:07 -0500

    added .js extension to utils/setUserKey

--- barista.main.js
+++ barista.main.js
@@ -5592,6 +5592,23 @@ Barista.getEmSizeInPixels = function(id) {
     var el = document.body;
     return Number(getComputedStyle(el, "").fontSize.match(/(\d+)px/)[1]);
 }
+// # **setUserKey**
+
+// a utility function to set a user_key attribute on the Barista object and set up
+// ajax calls to api.lincscloud.org to pass that user_key as a parameter
+
+// arguments
+// 
+// 1.  {string}  **key**  The user_key to use or a path to a JSON file containing a user_key attribute, defaults to *""*
+Barista.setUserKey = function(key) {
+	var key_request = $.getJSON(key);
+	key_request.done(function(res){
+		Barista.user_key = res.user_key;
+	});
+	key_request.fail(function(){
+		Barista.user_key = key;
+	});
+};
 // # **BarPlotModel**
 // A Backbone.Model to hold the information needed to make a simple bar plot.  The model includes a title,
 // axis title, data, data_labels, and an optional object for metadata on the points in the data.  The meta
