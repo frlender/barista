255aa0a302c864fbfe158be3d273baa371a71c27
Corey Flynn <flynn.corey@gmail.com>
2014-04-28 12:46:51 -0400

    added AnalysisHistoryCollection

--- source/collections/collections.main.js
+++ source/collections/collections.main.js
@@ -1,3 +1,84 @@
+// # **AnalysisHistoryCollection**
+// A Backbone.Collection that represents a set of analysis history objects.  This collection is suitable for
+// internal use in GridView.
+
+// optional arguments:
+
+// 1.  {Backbone.Model}  **model**  the model used for the collection objects. defaults to *PertModel*
+// 2.  {String}  **url**  the url from which model data is fetched. defaults  to *'http://api.lincscloud.org/a2/pertinfo?callback=?'*
+// 3.  {String}  **skip**  the skip parameter used in api calls when the collection is updated. defaults to *0*
+// 4.  {Boolean}  **isLoading**  indicates wether or not the collection is in the middle of a fetch operation. defaults to *false*
+
+Barista.Collections.AnalysisHistoryCollection = Backbone.Collection.extend({
+    // #### model
+    // the model used for the collection objects.
+    model: Backbone.Model,
+
+    // #### url
+    // the url from which model data is fetched
+    url: 'http://api.lincscloud.org/compute_status?callback=?',
+
+    // #### skip
+    // the skip parameter used in api calls when the collection is updated.
+    skip: 0,
+
+    // #### isLoading
+    // indicates wether or not the collection is in the middle of a fetch operation.
+    isLoading: false,
+
+    // ### maxCount
+    // the maximum size of the collection. defaults to Infinity
+    maxCount: Infinity,
+
+    // ## getData
+    // `AnalysisHistoryCollection.getData(search_string,search_type,limit)`
+
+    // Gets additional data from the specified url and stores them as models in the collection
+
+    // arguments
+    //
+    // 1.  {string}  **search\_string**  the string on which a regex search into the api at the collections url will be performed, defaults to *""*
+    // 2.  {string}  **search\_type**  the type of search that will be performed, defaults to *"single"*
+    // 3.  {number}  **limit**  the number of models to be fetched, defaults to *30*
+
+    getData: function(search_string,search_type,limit){
+        var self = this;
+        // set **isLoading** to true so we don't constantly make api calls before the data comes back
+        this.isLoading = true;
+
+        // store the value of **search\_string**, **search\_type**, and **limit** on the collection object
+        this.search_string = (search_string !== undefined) ? search_string : '';
+        this.search_type = (search_type !== undefined) ? search_type : '';
+        this.limit = (limit !== undefined) ? limit : 30;
+
+        // set up the query parameter for user_id
+        this.q_param = '{"user_id":"' + search_string + '"}';
+
+        // build a parameter object for the api call
+        var params = {q: this.q_param,
+            l: this.limit,
+            sk: this.skip};
+
+        // make the api call and store the results as individual models in the collection.
+        // we don't remove old models in this case as we want to support continuous building
+        // of the model list from a remote api.  On success, set **isLoading** back to false
+        $.getJSON(this.url, params, function(res){
+            self.set(res,{remove: false});
+            self.isLoading = false;
+        });
+
+        // make a second api call to find the maximum number of items in the collection
+        // and set that as an attribute on it
+        if (this.maxCount == Infinity){
+            params = _.omit(params,['l','sk']);
+            params = _.extend(params,{c: true});
+            $.getJSON(this.url,params,function(res){
+                self.maxCount = res.count;
+            });
+        }
+    }
+});
+
 // # **CellCollection**
 // A Backbone.Collection that represents a set of cell types.  This collection is suitable for 
 // internal use in GridView.
