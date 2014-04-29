e9d01c4d4f1c8b88f7048c898caf04d47bdce370
Corey Flynn <flynn.corey@gmail.com>
2014-04-29 10:42:39 -0400

    added GenericMongoModel and set it as the default for AnalysisHistoryCollection

--- source/barista.main.js
+++ source/barista.main.js
@@ -1368,9 +1368,12 @@ Barista.Models.GenericCountModel = Backbone.Model.extend({
 // are passed to the model as normal and a date attribute is set from the _id field of the mongo document
 // `pert_model = new GenericMongoModel()`
 Barista.Models.GenericMongoModel = Backbone.Model.extend({
-    defaults:{
-        date: new Date(parseInt(this.get("_id").substring(0,8), 16)*1000)
-    }
+    // ### initialize
+    // Overides the base Model's initialize method to add the models date attribute and set the cid to the mongo _id field
+    initialize: function(attributes, options) {
+        this.cid = this.get('_id')
+        this.set("date", new Date(parseInt(this.cid.substring(0,8), 16)*1000));
+  }
 });
 
 // # **HeatmapModel**
