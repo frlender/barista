b6e19b22b103d29fe733278efb0b50c4d80baa84
Corey Flynn <flynn.corey@gmail.com>
2014-04-29 09:46:33 -0400

    added GenericMongoModel and set it as the default for AnalysisHistoryCollection

---
+++ source/models/GenericMongoModel.js
@@ -0,0 +1,13 @@
+// # **GenericMongoModel**
+
+// A Backbone.Model that represents a generic MongoDB object.  All fields in the document
+// are passed to the model as normal and a date attribute is set from the _id field of the mongo document
+// `pert_model = new GenericMongoModel()`
+Barista.Models.GenericMongoModel = Backbone.Model.extend({
+    // ### initialize
+    // Overides the base Model's initialize method to add the models date attribute and set the cid to the mongo _id field
+    initialize: function(attributes, options) {
+        this.cid = this.get('_id')
+        this.date = new Date(parseInt(this.cid.substring(0,8), 16)*1000);
+  }
+});
