7493020d90dcc48c8f0a7091aa28c021f1e9143a
Corey Flynn <flynn.corey@gmail.com>
2014-04-30 12:19:01 -0400

    template adjustments

--- templates/barista_templates.js
+++ templates/barista_templates.js
@@ -112,7 +112,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</p>\n        <p class=\"visible-xs cmap-center-text-small cmap-card-title-text\">";
+    + "</p>\n        <p class=\"visible-xs cmap-center-text cmap-card-title-text-small\">";
   if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
