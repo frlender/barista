507c0930d6505be7a9165a1c3f55f41d9ed449eb
Corey Flynn <flynn.corey@gmail.com>
2014-05-05 10:52:05 -0400

    added AnalysisHistoryModel

--- barista.main.js
+++ barista.main.js
@@ -6932,7 +6932,7 @@ Barista.Models.TickModel = Backbone.Model.extend({
 Barista.Collections.AnalysisHistoryCollection = Backbone.Collection.extend({
     // #### model
     // the model used for the collection objects.
-    model: Barista.Models.GenericMongoModel,
+    model: Barista.Models.AnalsysHistoryModel,
 
     // #### url
     // the url from which model data is fetched
