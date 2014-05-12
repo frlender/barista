18afe6c90dc5c4b33c5b029d14aab83d284d1a56
Corey Flynn <flynn.corey@gmail.com>
2014-05-12 14:29:44 -0400

    updated added barista footer images

--- source/barista.js
+++ source/barista.js
@@ -3567,8 +3567,8 @@ Barista.Views.CMapFooterView = Backbone.View.extend({
 		// store passed parameters as attributes of the view
 		this.organization = (this.options.organization !== undefined) ? this.options.organization : "Broad Institute";
 		this.terms_url = (this.options.terms_url !== undefined) ? this.options.terms_url : "http://lincscloud.org/terms-and-conditions/";
-		this.logo = (this.options.logo !== undefined) ? this.options.logo : ['http://coreyflynn.github.io/Bellhop/img/broad_logo_small.png','http://coreyflynn.github.io/Bellhop/img/cmap_logo_small.png','http://coreyflynn.github.io/Bellhop/img/NIH_LINCS_logo.gif'];
-		this.logo_url = (this.options.logo_url !== undefined) ? this.options.logo_url : ['http://www.broadinstitute.org/','http://lincscloud.org/','http://www.lincsproject.org/'];
+		this.logo = (this.options.logo !== undefined) ? this.options.logo : ['http://coreyflynn.github.io/Bellhop/img/broad_logo_small_text.png','http://coreyflynn.github.io/Bellhop/img/CMap-logox.png','http://coreyflynn.github.io/Bellhop/img/NIH_LINCS_logo.gif','http://coreyflynn.github.io/Bellhop/img/skunkworks-logo.png'];
+		this.logo_url = (this.options.logo_url !== undefined) ? this.options.logo_url : ['http://www.broadinstitute.org/','http://lincscloud.org/','http://www.lincsproject.org/','http://www.broadinstitute.org/vis'];
 		this.template = (this.options.template !== undefined) ? this.options.template : "templates/CMapFooter.handlebars";
 
 		// compile the default template for the view
