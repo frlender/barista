9f14db131c92ab1a296981628ba38751ce4ea01c
Corey Flynn <flynn.corey@gmail.com>
2014-08-01 12:34:33 -0400

    opacity animation in treemap->draw_foreignObject

--- source/views/views.main.js
+++ source/views/views.main.js
@@ -2255,6 +2255,8 @@ Barista.Views.FlatTreeMapView = Backbone.View.extend({
 			.style("height","100%")
 			.style("width","100%")
 			.style("display","flex")
+			.style("opacity",0)
+			.transition().duration(500).syle("opacity",1)
 			.html(function(d){
 				if (d.children === undefined){
 					return self.category_html[d._id];
