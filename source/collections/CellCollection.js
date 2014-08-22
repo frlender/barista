4529456fabba487e93ae5809634537009cd39045
Corey Flynn <flynn.corey@gmail.com>
2014-08-22 16:47:59 -0400

    CellCollection url on init

--- source/collections/CellCollection.js
+++ source/collections/CellCollection.js
@@ -15,13 +15,16 @@
                                           // isLoading: false});`
 
 Barista.Collections.CellCollection = Backbone.Collection.extend({
+    // #### initialize
+    initialize: function(){
+        #### initialize
+        // the url from which model data is fetched
+        this.url =  Barista.APIURL + '/a2/cellinfo?callback=?';
+    },
+
     // #### model
     // the model used for the collection objects.
     model: Barista.Models.CellModel,
-
-    // #### url
-    // the url from which model data is fetched
-    url: Barista.APIURL + '/a2/cellinfo?callback=?',
 
     // #### skip
     // the skip parameter used in api calls when the collection is updated.
