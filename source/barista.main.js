2d9c0906c7dc95739eea494f029833dc77a257a9
Corey Flynn <flynn.corey@gmail.com>
2014-08-22 13:46:58 -0400

    setUserKey console log

--- source/barista.main.js
+++ source/barista.main.js
@@ -1611,6 +1611,7 @@ Barista.setUserKey = function(key) {
 		Barista.user_key = res.user_key;
 	});
 	key_request.fail(function(){
+		console.log("could not find a file at " + key + "; setting Barista.user_key to " + key);
 		Barista.user_key = key;
 	});
 };
