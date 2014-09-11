d426d0fc022f73f0a352e08ff14abae78a8acc4d
Corey Flynn <flynn.corey@gmail.com>
2014-09-11 16:52:51 -0400

    cmap_grid scss

--- barista.main.js
+++ barista.main.js
@@ -6805,7 +6805,7 @@ Barista.Models.AnalysisHistoryModel = Backbone.Model.extend({
         }else{
             this.set({view_result_link: ''});
         }
-        this.set({delete_button: '<i class="fa fa-times-circle .cmap-grid-delete-button" onclick="Backbone.trigger(' + "'AnalysisHistoryModel:delete'" + ',' + "'" +  this.get("job_id") + "'" + ')"></i>'});
+        this.set({delete_button: '<i class="fa fa-times-circle cmap-grid-delete-button" onclick="Backbone.trigger(' + "'AnalysisHistoryModel:delete'" + ',' + "'" +  this.get("job_id") + "'" + ')"></i>'});
   }
 });
 
