91dc674ebbf575d4db5570e30307913da7bf349c
Corey Flynn <flynn.corey@gmail.com>
2014-11-04 09:17:30 -0500

    header splitter css

--- barista.main.js
+++ barista.main.js
@@ -5350,7 +5350,7 @@ function program1(depth0,data) {
 function program3(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n						<div class=\"dropdown pull-right\">\n							<p class=\"cmap-header-username data-toggle\"><span class=\"caret\"></span></p>\n							<p id=\"username\" title=\"profile\" class=\"cmap-header-username data-toggle\" data-toggle=\"dropdown\">";
+  buffer += "\n						<div class=\"dropdown pull-right\">\n							<p id=\"username\" title=\"profile\" class=\"cmap-header-username data-toggle\" data-toggle=\"dropdown\">";
   if (stack1 = helpers.user) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.user; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
