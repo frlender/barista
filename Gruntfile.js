233091f34be0c9c8d9b3e37c38a94e8c5cd24700
Corey Flynn <flynn.corey@gmail.com>
2014-11-05 14:40:21 -0500

    bootstrap select debugging: add hogan

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,7 +77,7 @@ module.exports = function(grunt) {
             //   'bower_components/Blob/Blob.js',
             //   'bower_components/canvas-toBlob.js/canvas-toBlob.js',
             //   'external_source/typeahead.min.js',
-            //   'bower_components/hogan/web/builds/2.0.0/hogan-2.0.0.js',
+              'bower_components/hogan/web/builds/2.0.0/hogan-2.0.0.js',
             //   'bower_components/jquery-mousewheel/jquery.mousewheel.min.js',
             //   'bower_components/jquery.cookie/jquery.cookie.js',
             //   'bower_components/offline/offline.min.js'
