00d45a1c2aa67f7c8f500a8cf2d8923acd5933bf
Corey Flynn <flynn.corey@gmail.com>
2015-01-23 13:35:44 -0500

    sequenceModel parseSequence

--- barista.main.js
+++ barista.main.js
@@ -5180,9 +5180,56 @@ Barista.Models.SequenceModel = Backbone.Model.extend({
    * @type {Object}
    */
   defaults: {
-    rawSequence: '',
+    sequence: '',
     displaySequence: '',
     modifications: new Backbone.Collection()
+  },
+
+  /**
+   * initialize the model to listen to its own model changes
+   */
+  initialize: function() {
+    this.listenTo(this, 'change:sequence', this.parseSequence);
+  },
+
+  /**
+   * function to populate the derived model attributes of
+   * displaySequence and modifications
+   */
+  parseSequence: function() {
+    var sequence = this.get('sequence'),
+        modifications = this.get('modifications'),
+        indices = [],
+        numMods = 0;
+        modObjects = [];
+
+    // reset the modifications collection
+    modifications.reset();
+
+    // remove leading and trailing underscores
+    sequence.replace(/_/g,'');
+
+    // find all occurances of '(' in the sequence, these are the modifications
+    for(var i=0; i<sequence.length;i++) {
+      if (sequence[i] === '(') {
+        indices.push(i);
+      }
+    }
+
+    // for each modification, pop it out of the sequence string and add a new
+    // model to the modifications collection
+    indices.forEach(function(index) {
+      var mod;
+      index -= numMods * 4;
+      mod = sequence.slice(index + 1,index + 3);
+      sequence = [sequence.slice(0,index),sequence.slice(index + 4,sequence.length)].join('');
+      modObjects.push({index: index, modification: mod});
+    });
+
+    modifications.add(modObjects);
+
+    // set the derived attributes on the model
+    this.set({'modifications': modifications, 'displaySequence': sequence});
   }
 });
 
