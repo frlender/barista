eed19f0c259de371bc435eaa9830503a7f38540f
Corey Flynn <flynn.corey@gmail.com>
2015-02-05 10:30:34 -0500

    undefined render bug in PertCountView

--- source/barista.js
+++ source/barista.js
@@ -1262,10 +1262,12 @@ Barista.getQueryVariable = function(variable)
 // credit to Elias Zamaria http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
 Barista.numberWithCommas = function(x){
     // first check to see if something was passed. if so, convert it
-    if (x == 0) {
-      return '0';
-    } else {
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    if (x){
+      if (x == 0) {
+        return '0';
+      } else {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      }
     }
 };
 
