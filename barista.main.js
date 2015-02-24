68a2d2c6ac16e1fcda8d00faf1dc227e1a3e672a
Corey Flynn <flynn.corey@gmail.com>
2015-02-24 12:38:42 -0500

    updated generic count model

--- barista.main.js
+++ barista.main.js
@@ -4732,70 +4732,42 @@ Barista.Models.GeneDetailModel = Backbone.Model.extend({
 // optional arguments:
 
 // 1.  {string}  **search_field**  the document field the model with count over upon fetching data, defaults to *"pert_iname"*
-// 2.  {string}  **url**  the url of the api service to fetch data from, defaults to *"//api.lincscloud.org/a2/pertinfo"*
+// 2.  {string}  **url**  the url of the api service to fetch data from, defaults to *"http://api.lincscloud.org/a2/pertinfo"*
 
 // `generic_count_model = new GenericCountModel()`
-
 Barista.Models.GenericCountModel = Backbone.Model.extend({
-  // ### defaults
-  // describes the model's default parameters
 
-  // 1.  {string}  **search_field**  the document field the model with count over upon fetching data, defaults to *"pert_iname"*
-  // 2.  {string}  **url**  the url of the api service to fetch data from, defaults to *"//api.lincscloud.org/a2/pertinfo"*
-  defaults: {
-    "search_field": "pert_iname",
-    "url": "//api.lincscloud.org/a2/pertinfo",
-    "count": 0,
-    "last_update": (new Date()).getTime(),
-    "search_string": "",
-    "distinct": false
+  defaults:{
+    count:0,
+
   },
 
-  // ## initialize
-  // custom initialization to make sure we have the correct url for jsonp
-  /**
-   * custom initialization to make sure we have the correct url for jsonp
-   */
-  initialize: function(){
-    var re = new RegExp("/?callback=/?");
-    if (!re.test(this.get("url"))){
-      this.set({"url": this.get("url") + "?callback=?"});
-    }
-  },
-
-  // ### fetch
-  // fetches new data from the API.  the count is updated with a new
-  // count based on the results of the api call
-  /**
-   * fetches new data from the API
-   * @param  {string}  search_string  value to search for
-   */
-  fetch: function(search_string){
-    // update the model's search string attribute
-    this.set("search_string",search_string);
-
-    // set up API call parameters
-    search_string = (search_string[0] === "*") ? search_string.replace("*",".*") : search_string;
-    var params = {q:'{"' + this.get("search_field") + '":{"$regex":"^' + search_string + '","$options":"i"}}',
-              c:true};
-    if (this.get("distinct")){
-        _.extend(params,{d:this.get("search_field")});
-    }
-
-    // run the api request
-    var self = this;
-    var count;
-    $.getJSON(this.get("url"),params,function(res) {
-      if (res === 0){
-        count = 0;
-      }else{
-        count = res.count;
-      }
-      var t = (new Date()).getTime();
-      self.set({count: count,last_update: t});
+  initialize:function(attrs){
+    this.listenTo(attrs.source,'update',function(){
+      if(attrs.source.getCount(attrs.key))
+      this.set('count',attrs.source.getCount(attrs.key));
+      else this.set('count',0);
     });
   }
 });
+
+Barista.Models.GenericSourceModel = Backbone.Model.extend({
+  initialize:function(attrs){
+    this.url = 'http://10.125.171.42:8080/' + attrs.key;
+  },
+
+  getCount:function(inputKey){
+    return this.data[inputKey.group][inputKey.name];
+  },
+
+  update:function(){
+    var self = this;
+    $.getJSON(this.url,function(data){
+      self.data = data;
+      self.trigger('update');
+    })
+  }
+})
 
 // # **GenericMongoModel**
 
