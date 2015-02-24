c415216a080236190e313631b8b356645509d112
Corey Flynn <flynn.corey@gmail.com>
2015-02-23 21:15:30 -0500

    generic count view label weight

--- source/barista.js
+++ source/barista.js
@@ -5614,7 +5614,7 @@ Barista.Views.GenericCountView = Barista.Views.BaristaBaseView.extend({
       .attr("class","genericCountViewLabel")
       .attr("y",25)
       .attr("font-family","'Open Sans")
-      .attr("font-weight","700")
+      .attr("font-weight","300")
       .attr("font-size","16px")
       .attr("fill",'#222222')
       .text(this.label);
